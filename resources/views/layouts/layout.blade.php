a87c955f2c9a0ba6e62eddd351dc313a97ce13dd
Andrea Creazzi <andreacreazzi@libero.it>
2023-08-03 14:34:39 +0200

    add img folder + add layouts folder

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/png">
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+
+</body>
+
+</html>
